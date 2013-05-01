60273fc3eb42ee5cc4a9bb103a40c12f5d3630a9
slopjong <git@slopjong.de>
2013-05-01 21:24:46 +0200

    Link fix, output as image added

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-git clone git://github.com/finwe/mpdf.git
-mv fonts/sansation/*.ttf mpdf/ttfonts
-mv fonts/noto_sans/*.ttf mpdf/ttfonts
+git clone --depth=1 git://github.com/finwe/mpdf.git
+cp fonts/sansation/*.ttf mpdf/ttfonts
+cp fonts/noto_sans/*.ttf mpdf/ttfonts
+
+# remove the closing php tag as this is bad style
+sed -i "s/\?>//g" mpdf/config_fonts.php
+
+# add the new fonts to the configuration files
+echo "\$this->fontdata['sansation'] = array( 'R' => 'Sansation_Regular.ttf', 'B' => 'Sansation_Bold.ttf', 'I' => 'Sansation_Italic.ttf', 'BI' => 'Sansation_Bold_Italic.ttf');" >> mpdf/config_fonts.php
+echo "\$this->fontdata['notosans'] = array( 'R' => 'NotoSans-Regular.ttf', 'B' => 'NotoSans-Bold.ttf', 'I' => 'NotoSans-Italic.ttf', 'BI' => 'NotoSans-BoldItalic.ttf');" >> mpdf/config_fonts.php
